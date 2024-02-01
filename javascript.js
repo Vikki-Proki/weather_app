f4c57f24c5c87b18454b4141cf7f38a72415f306
Vikki_Proki <154062268+Vikki-Proki@users.noreply.github.com>
2024-02-01 15:37:54 +0200

    added changes for search engine

--- javascript.js
+++ javascript.js
@@ -1,33 +1,62 @@
 function currentWeather(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
+  let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
   descriptionElement = response.data.condition.description;
-  humidity.innerHTML = response.data.temperature.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}">`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-  let apiKey = edo97t5ec7b58060fa4428df5feafb3a;
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+  let apiKey = "edo97t5ec7b58060fa4428df5feafb3a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(searchCity);
+  axios.get(apiUrl).then(currentWeather);
 }
 
-function search(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
 }
 let searchInput = document.querySelector("#search-form-input");
 searchCity(searchInput.value);
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("click", search);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Kyiv");
